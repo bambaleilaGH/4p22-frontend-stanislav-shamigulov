273b847a176c48dbe764ee962b13c12b2792c94f
unknown <appcampdev@gmail.com>
2022-11-04 18:57:27 +0500

    Add homework_9

--- homework_9/script.js
+++ homework_9/script.js
@@ -1,10 +1,23 @@
-
+'use strict'
 
 function makeFibonacciFunction() {
-    
-    
-}
-
+    let f0 = 1;
+    let f1 = -1;
+    return function() {
+        let f = f0 + f1;
+        f1 = f0;
+        f0 = f;
+        console.log(f);
+    }
+}   
 
 const fibonacci = makeFibonacciFunction();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+fibonacci();
+
+
 
